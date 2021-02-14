984b1ca493e1943df89c0ec90b053fbfb9d72ee8
Fl@me <evgene.fe@gmail.com>
2021-02-14 17:39:39 +0300

    adoptive header-block

--- static/js/script.js
+++ static/js/script.js
@@ -15,13 +15,16 @@ $(document).ready(function(){
     let link = $('.menu-link');
     let link_active = $('.menu-link_active');
     let menu = $('.menu');
+  
 
     link.click(function() {
         link.toggleClass('menu-link_active');
         menu.toggleClass('menu_active');
+  
     });
     link_active.click(function() {
         link.removeClass('menu-link_active');
+
     });
 });
 
